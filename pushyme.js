51d07cacebbdfc8bf3c04ff778b11fdfd5a19c3a
tmh1982 <aa@aa.aa>
2016-02-15 11:30:02 +0800

    pushy

--- pushyme.js
+++ pushyme.js
@@ -1,11 +1,8 @@
 cordova.define("com.ospicon.pushy.pushme", function(require, exports, module) { 
 var exec = require('cordova/exec');
-
 var pushyMeTokenID = function(callbacksucess,callbackfail) {
     exec(callbacksucess,callbackfail,"CDVPlushyMePlugin", "pushyMeTokenID", []);
 };
-
 module.exports = pushyMeTokenID;
-console.log("pushy loaded");
 });
 
